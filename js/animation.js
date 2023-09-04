e30471495d3ac6a004840dc8fa2563d1e8e0bd6a
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-09-04 12:34:06 +0530

    content updated

--- js/animation.js
+++ js/animation.js
@@ -8,3 +8,15 @@ const observer = new IntersectionObserver ((entries) => {
 
 const hidden_up_downElements = document.querySelectorAll ('.hidden-up-down');
 hidden_up_downElements.forEach ((el) => observer.observe(el));
+
+
+const observer1 = new IntersectionObserver ((entries) => {
+    entries.forEach ((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add ('show');
+        }
+    });
+});
+
+const hidden_down_upElements = document.querySelectorAll ('.hidden-down-up');
+hidden_down_upElements.forEach ((el1) => observer.observe(el1));
